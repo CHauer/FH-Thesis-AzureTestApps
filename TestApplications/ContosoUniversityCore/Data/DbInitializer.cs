8c35c5b2e5c749e450740e34d703e456211f682e
Christoph Hauer <hauer.christoph@live.at>
2018-05-16 01:42:52 +0200

    First Strep Load Tests

--- TestApplications/ContosoUniversityCore/Data/DbInitializer.cs
+++ TestApplications/ContosoUniversityCore/Data/DbInitializer.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,6 +10,13 @@ namespace ContosoUniversityCore.Data
 {
     public static class DbInitializer
     {
+        private static Random random = new Random(DateTime.Now.Millisecond);
+        private static List<string> firstNames = new List<string> { "Abigail ", "Alexandra ", "Alison ", "Amanda ", "Amelia ", "Amy ", "Andrea ", "Angela ", "Anna ", "Anne ", "Audrey ", "Ava ", "Bella ", "Bernadette ", "Carol ", "Caroline ", "Carolyn ", "Chloe ", "Claire ", "Deirdre ", "Diana ", "Diane ", "Donna ", "Dorothy ", "Elizabeth ", "Ella ", "Emily ", "Emma ", "Faith ", "Felicity ", "Fiona ", "Gabrielle ", "Grace ", "Hannah ", "Heather ", "Irene ", "Jan ", "Jane ", "Jasmine ", "Jennifer ", "Jessica ", "Joan ", "Joanne ", "Julia ", "Karen ", "Katherine ", "Kimberly ", "Kylie ", "Lauren ", "Leah ", "Lillian ", "Lily ", "Lisa ", "Madeleine ", "Maria ", "Mary ", "Megan ", "Melanie ", "Michelle ", "Molly ", "Natalie ", "Nicola ", "Olivia ", "Penelope ", "Pippa ", "Rachel ", "Rebecca ", "Rose ", "Ruth ", "Sally ", "Samantha ", "Sarah ", "Sonia ", "Sophie ", "Stephanie ", "Sue ", "Theresa ", "Tracey ", "Una ", "Vanessa ", "Victoria ", "Virginia ", "Wanda ", "Wendy ", "Yvonne ", "Zoe ", "Adam ", "Adrian ", "Alan ", "Alexander ", "Andrew ", "Anthony ", "Austin ", "Benjamin ", "Blake ", "Boris ", "Brandon ", "Brian ", "Cameron ", "Carl ", "Charles ", "Christian ", "Christopher ", "Colin ", "Connor ", "Dan ", "David ", "Dominic ", "Dylan ", "Edward ", "Eric ", "Evan ", "Frank ", "Gavin ", "Gordon ", "Harry ", "Ian ", "Isaac ", "Jack ", "Jacob ", "Jake ", "James ", "Jason ", "Joe ", "John ", "Jonathan ", "Joseph ", "Joshua ", "Julian ", "Justin ", "Keith ", "Kevin ", "Leonard ", "Liam ", "Lucas ", "Luke ", "Matt ", "Max ", "Michael ", "Nathan ", "Neil ", "Nicholas ", "Oliver ", "Owen ", "Paul ", "Peter ", "Phil ", "Piers ", "Richard ", "Robert ", "Ryan ", "Sam ", "Sean ", "Sebastian ", "Simon ", "Stephen ", "Steven ", "Stewart ", "Thomas ", "Tim ", "Trevor ", "Victor ", "Warren ", "William" };
+        private static List<string> lastNames = new List<string> { "Abraham ", "Allan ", "Alsop ", "Anderson ", "Arnold ", "Avery ", "Bailey ", "Baker ", "Ball ", "Bell ", "Berry ", "Black ", "Blake ", "Bond ", "Bower ", "Brown ", "Buckland ", "Burgess ", "Butler ", "Cameron ", "Campbell ", "Carr ", "Chapman ", "Churchill ", "Clark ", "Clarkson ", "Coleman ", "Cornish ", "Davidson ", "Davies ", "Dickens ", "Dowd ", "Duncan ", "Dyer ", "Edmunds ", "Ellison ", "Ferguson ", "Fisher ", "Forsyth ", "Fraser ", "Gibson ", "Gill ", "Glover ", "Graham ", "Grant ", "Gray ", "Greene ", "Hamilton ", "Hardacre ", "Harris ", "Hart ", "Hemmings ", "Henderson ", "Hill ", "Hodges ", "Howard ", "Hudson ", "Hughes ", "Hunter ", "Ince ", "Jackson ", "James ", "Johnston ", "Jones ", "Kelly ", "Kerr ", "King ", "Knox ", "Lambert ", "Langdon ", "Lawrence ", "Lee ", "Lewis ", "Lyman ", "MacDonald ", "Mackay ", "Mackenzie ", "MacLeod ", "Manning ", "Marshall ", "Martin ", "Mathis ", "May ", "McDonald ", "McLean ", "McGrath ", "Metcalfe ", "Miller ", "Mills ", "Mitchell ", "Morgan ", "Morrison ", "Murray ", "Nash ", "Newman ", "Nolan ", "North ", "Ogden ", "Oliver ", "Paige ", "Parr ", "Parsons ", "Paterson ", "Payne ", "Peake ", "Peters ", "Piper ", "Poole ", "Powell ", "Pullman ", "Quinn ", "Rampling ", "Randall ", "Rees ", "Reid ", "Roberts ", "Robertson ", "Ross ", "Russell ", "Rutherford ", "Sanderson ", "Scott ", "Sharp ", "Short ", "Simpson ", "Skinner ", "Slater ", "Smith ", "Springer ", "Stewart ", "Sutherland ", "Taylor ", "Terry ", "Thomson ", "Tucker ", "Turner ", "Underwood ", "Vance ", "Vaughan ", "Walker ", "Wallace ", "Walsh ", "Watson ", "Welch ", "White ", "Wilkins ", "Wilson ", "Wright ", "Young" };
+
+        private static int firstNamesMax;
+        private static int lastNamesMax;
+
         public static void Initialize(SchoolContext context)
         {
             //context.Database.EnsureCreated();
@@ -19,7 +27,7 @@ namespace ContosoUniversityCore.Data
                 return;   // DB has been seeded
             }
 
-            var students = new Student[]
+            var students = new List<Student>
             {
                 new Student { FirstMidName = "Carson",   LastName = "Alexander",
                     EnrollmentDate = DateTime.Parse("2010-09-01") },
@@ -36,8 +44,22 @@ namespace ContosoUniversityCore.Data
                 new Student { FirstMidName = "Laura",    LastName = "Norman",
                     EnrollmentDate = DateTime.Parse("2013-09-01") },
                 new Student { FirstMidName = "Nino",     LastName = "Olivetto",
-                    EnrollmentDate = DateTime.Parse("2005-09-01") }
+                    EnrollmentDate = DateTime.Parse("2005-09-01") },
+                new Student{ FirstMidName="Christoph",   LastName="Hauer",
+                    EnrollmentDate =DateTime.Parse("2005-09-01")},
+                new Student{ FirstMidName="Max",         LastName="Mustermann",
+                    EnrollmentDate =DateTime.Parse("2005-09-01")}
             };
+
+            for (int i = 0; i < 400; i++)
+            {
+                students.Add(new Student()
+                {
+                    FirstMidName = firstNames[random.Next(0, firstNamesMax)],
+                    LastName = lastNames[random.Next(0, lastNamesMax)],
+                    EnrollmentDate = DateTime.Now
+                });
+            }
 
             foreach (Student s in students)
             {
