780bd992c5b2228d00887b482523a0596cb7c3e1
Christoph Hauer <hauer.christoph@live.at>
2018-05-21 16:23:37 +0200

    load test

--- TestApplications/LoadTestAppA/ExtractLiveMetrics.js
+++ TestApplications/LoadTestAppA/ExtractLiveMetrics.js
@@ -1,23 +1,42 @@
 ﻿window.setInterval(extractData, 500);
 
 function extractData() {
+    getServerData("even");
+    getServerData("odd");
+}
+
+function getServerData(serverrow) {
     var memoryValue = null;
     var cpuValue = null;
     var requestValue = null;
     var requestFailValue = null;
     var timestamp = null;
+    var server = null;
 
     var serverData = document.getElementById("serversInfoGrid");
+    var serverInstances = serverData.getElementsByClassName("ui-widget-content slick-row " + serverrow);
 
-    var requestNodes = serverData.getElementsByClassName("slick-cell l1 r1 slick-number-column");
-    var requestFailNodes = serverData.getElementsByClassName("slick-cell l2 r2 slick-number-column");
-    var cpuNodes = serverData.getElementsByClassName("slick-cell l3 r3 slick-number-column");
-    var memoryNodes = serverData.getElementsByClassName("slick-cell l4 r4 slick-number-column");
+    if(serverInstances == null || serverInstances.length <= 0){
+        return;
+    }
+
+    var serverInstance = serverInstances[0];
+
+    var serverNodes  = serverInstance.getElementsByClassName("slick-cell l0 r0");
+    var requestNodes = serverInstance.getElementsByClassName("slick-cell l1 r1 slick-number-column");
+    var requestFailNodes = serverInstance.getElementsByClassName("slick-cell l2 r2 slick-number-column");
+    var cpuNodes = serverInstance.getElementsByClassName("slick-cell l3 r3 slick-number-column");
+    var memoryNodes = serverInstance.getElementsByClassName("slick-cell l4 r4 slick-number-column");
 
     //console.log(cpuNodes.length);
 
-    if (cpuNodes != null && cpuNodes.length >= 1 && requestNodes != null && requestNodes.length >= 1
-        && requestFailNodes != null && requestFailNodes.length >= 1 && memoryNodes != null && memoryNodes.length >= 1) {
+    if (cpuNodes != null && cpuNodes.length >= 1 &&
+        requestNodes != null && requestNodes.length >= 1 && 
+        requestFailNodes != null && requestFailNodes.length >= 1 &&
+        memoryNodes != null && memoryNodes.length >= 1 && 
+        serverNodes != null && serverNodes.length >= 1) {
+
+        server = serverNodes[0].innerHTML;
         cpuValue = cpuNodes[0].innerHTML;
         requestValue = requestNodes[0].innerHTML;
         requestFailValue = requestFailNodes[0].innerHTML;
@@ -40,11 +59,12 @@ function extractData() {
         //});
 
         console.log({
-            timestamp: timestamp,
+            server: server,
+            time: timestamp,
             cpu: cpuValue,
-            memory: memoryValue,
-            requests: requestValue,
-            requestsFailed: requestFailValue
+            ram: memoryValue,
+            req: requestValue,
+            //reqFail: requestFailValue
         });
     }
 }
